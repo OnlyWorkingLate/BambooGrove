e1e2d1b316bb1d47961bc6656861eb8fb5d585a3
Ahn Jae Wook <ajw4586@naver.com>
2016-07-22 21:15:28 +0900

    mysql basic setting and add cms base templete

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
+'use strict';
 // NOTE: 환경변수를 설정하고 테스트바람.
 /*
- PORT        실행될 포트번호. 기본 5500번
- MONGO_URI   DB연결 URI. 없이 실행시키면 연결 오류.
+ BAMBOO_PORT        실행될 포트번호.
  NODE_ENV 서버 실행 모드.
  development / production / maintance 세 가지가 있고, development에서는 DB연결을 하지 않는다.
+ DB_USER mySQL db user
+ DB_PASSWORD mySQL db password
  */
-'use strict';
 const express = require('express'),
     mongoose = require('mongoose'),
+    mysql = require('mysql'),
     bodyParser = require('body-parser'),
     morgan = require('morgan'),
     favicon = require('serve-favicon'),
     ejs = require('ejs');
 
+let db_connection = mysql.createConnection({
+  host: 'jaewook.me',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: 'bamboo'
+});
+
 let app = express();
 
 //  For environment value check
 console.log('NODE_ENV is ' + app.get('env'));
-console.log('PORT is ' + process.env.PORT);
-console.log('MONGO_URI is ' + process.env.MONGO_URI);
+console.log('PORT is ' + process.env.BAMBOO_PORT);
+console.log('DB_USER is ' + process.env.DB_USER);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.set('port', process.env.BAMBOO_PORT);
 
 app.set('view engine', 'ejs');
-app.set('PORT', process.env.PORT || 4586);
+app.set('PORT', app.get('port') || 8081);
 app.set('views', __dirname + '/views');
 
 //  set routes
@@ -36,25 +46,21 @@ app.use('/page', require('./routes/page'));
 //  start server with environment value 'NODE_ENV'.
 switch(app.get('env')) {
     case 'development':
-        app.listen(app.get('PORT'), () => {
-            console.log('BambooGrove server has been started without db connection at port ' + app.get('PORT'));
+        app.listen(app.get('port'), () => {
+            console.log('BambooGrove server has been started without db connection at port ' + app.get('port'));
         });
         break;
     case 'maintance':
         break;
     case 'production':
     default:
-        mongoose.connect(process.env.MONGO_URI);
-        let connection = mongoose.connection;
-        //  DB connect error handle
-        connection.on('errror', (err) => {
-            console.log('Error connecting DB. + Err: ' + err);
-            return;
-        });
-        //  run server
-        connection.on('open', () => {
-            app.listen(app.get('PORT'), () => {
-                console.log('BambooGrove server has been started at port ' + app.get('PORT'));
+        db_connection.connect((err) => {
+            if(err) {
+                console.error('error connecting to db.\n' + err.stack);
+                return;
+            }
+            app.listen(app.get('port'), (err) => {
+                console.log('Server was started at port at ' + app.get('port'));
             });
         });
         break;
