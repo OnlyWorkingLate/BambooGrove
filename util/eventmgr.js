978cc654260dc8ab29d14b8616fe352c995d6a57
Ahn Jae WOok <ajw4586@naver.com>
2016-10-01 03:09:03 +0900

    change: eventmgr changed.

--- util/eventmgr.js
+++ util/eventmgr.js
@@ -1,16 +1,17 @@
 //  서버의 이벤트 처리를 담당하는 모듈입니다.
 'use strict';
 
-const eventEmitter = require('events').EventEmitter;
+const EventEmitter = require('events').EventEmitter;
+let emitter = new EventEmitter();
 
 exports.db_connect = () => {
-    eventEmitter.emit('db_connect');
+    emitter.emit('db_connect');
 };
 exports.db_open = (cb) => {
-    eventEmitter.once('db_open', cb);
+    emitter.once('db_open', cb);
 };
 exports.db_error = (cb) => {
-    eventEmitter.on('db_error', cb);
+    emitter.on('db_error', cb);
 };
 
-exports = eventEmitter;
+exports = emitter;
